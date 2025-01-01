67788d2b2b44b7338571c3b2d071acc98a34ad90
abdellah <abdellahedaoudi80@gmail.com>
2025-01-01 02:47:01 +0100

    Refactor bootstrap function to use environment variable for port configuration

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    rawBody:true,
-    bodyParser:true,
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    bodyParser: true,
   });
-  app.setGlobalPrefix("/api")
-  await app.listen(3000);
+  app.setGlobalPrefix("/api");
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
+
+export default bootstrap;
