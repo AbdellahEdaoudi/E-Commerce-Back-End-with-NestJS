0a00dc8051e73cbf43ba4b5179cf6f2961e11347
abdellah <abdellahedaoudi80@gmail.com>
2025-01-01 03:19:45 +0100

    Update Vercel configuration and modify bootstrap to listen on dynamic port

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
     bodyParser:true,
   });
   app.setGlobalPrefix("/api")
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-export const handler = bootstrap;
+bootstrap();
