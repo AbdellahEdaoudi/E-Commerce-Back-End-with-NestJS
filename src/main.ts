30ea26dba8934e4143ceb2397e5cf343347c05f2
abdellah <abdellahedaoudi80@gmail.com>
2025-01-01 03:08:09 +0100

    Refactor Vercel configuration to use source TypeScript files and update bootstrap function to export handler

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-    bodyParser: true,
+  const app = await NestFactory.create(AppModule,{
+    rawBody:true,
+    bodyParser:true,
   });
-  app.setGlobalPrefix("/api");
-  await app.listen(process.env.PORT || 3000);
+  app.setGlobalPrefix("/api")
+  await app.listen(3000);
 }
 
-bootstrap();
-
-export default bootstrap;
+export const handler = bootstrap;
